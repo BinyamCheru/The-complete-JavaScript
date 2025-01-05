78dba417ba2d247132600e89a4cd7e3782700a50
Bina <benjaminbinyam500@gmail.com>
2025-01-05 20:49:14 +0300

    modified Conditional Statements

--- Conditional-Statements/app.js
+++ Conditional-Statements/app.js
@@ -9,3 +9,23 @@ if (value2) {
 } else {
   console.log('hello people');
 }
+
+// else if and !
+
+const num1 = 6;
+const num2 = 6;
+const result = num1 >= num2;
+
+const value1 = false;
+
+if (!value1) {
+  console.log('value is false');
+}
+
+// if (num1 > num2) {
+//   console.log('first number is bigger than second');
+// } else if (result) {
+//   console.log('first number equal to a second');
+// } else {
+//   console.log('second number is bigger than first');
+// }
